48559da6cc8706346796bdf6ec95a718052b7a58
RLCJ <2601857@sit.singaporetech.edu.sg>
2026-09-23 01:06:49 +0800

    Second Push

--- ryan.py
+++ ryan.py
@@ -1,5 +1,6 @@
-new = input("What is your name? ")
-while not new.isalpha():
+new_user = input("What is your name? ")
+while not new_user.isalpha():
     print("Please enter a valid name containing only letters!")
-    new = input("What is your name? ")
-print("Hello, " + new.strip() + "! Welcome!")
+    new_user = input("What is your name? ")
+print("Hello, " + new_user.strip() + "! Welcome!")
+
